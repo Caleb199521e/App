920c80c8532fba4f48714badeb213b06afb42b0c
CALEB AMPONG <campong004@st.ug.edu.gh>
2025-03-06 17:11:06 +0000

    Update run configuration to use environment variable for port

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
+import os
 from app import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 10000))  # Get PORT from environment or default to 10000
+    app.run(host="0.0.0.0", port=port, debug=True)
